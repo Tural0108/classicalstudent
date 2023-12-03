7c2be71a345749c5b2f336c7349ad7e4ee31438b
classicalstudent <nastya0108@bk.ru>
2023-12-03 20:54:34 +0300

    добавлена программа 6

---
+++ Prog_6/build/CMakeFiles/Prog_5.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Prog_5.dir/main.cpp.obj"
+  "CMakeFiles/Prog_5.dir/main.cpp.obj.d"
+  "Prog_5.exe"
+  "Prog_5.exe.manifest"
+  "Prog_5.pdb"
+  "libProg_5.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Prog_5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Prog_6/build/CMakeFiles/Prog_5.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Prog_5.
+# This may be replaced when dependencies are built.
